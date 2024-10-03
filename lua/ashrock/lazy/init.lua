aa9d6ef99522adbb8bc5e057fd6e9bcb6f612679
OnLee3 <dlwotjr2@gmail.com>
2024-10-03 17:23:03 +0900

    주석 삭제

--- lua/ashrock/lazy/init.lua
+++ lua/ashrock/lazy/init.lua
@@ -2,11 +2,6 @@ return {
     "nvim-lua/plenary.nvim",
     "yorumicolors/yorumi.nvim",
     'tpope/vim-fugitive',
-    -- 'neovim/nvim-lspconfig',
-    -- 'williamboman/mason.nvim',
-    -- 'williamboman/mason-lspconfig.nvim',
-    -- 'hrsh7th/nvim-cmp',
-    -- 'hrsh7th/cmp-nvim-lsp',
     {
         'nvim-telescope/telescope.nvim', tag = '0.1.8',
         dependencies = { 'nvim-lua/plenary.nvim' }
@@ -14,8 +9,5 @@ return {
     {
         "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
     },
-    -- {
-    --     'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'
-    -- },
 }
 
