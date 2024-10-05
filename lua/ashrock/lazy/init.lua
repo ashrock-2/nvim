e4c63d9a37f9d7ecce68ec65bdf1f688517b49ce
이재석 <jslee01@miridih.com>
2024-10-05 13:13:26 +0900

    feat: add treesitter config

--- lua/ashrock/lazy/init.lua
+++ lua/ashrock/lazy/init.lua
@@ -1,24 +1,21 @@
 return {
-    "nvim-lua/plenary.nvim",
-    'tpope/vim-fugitive',
-    'prettier/vim-prettier',
-    {
-        "yorumicolors/yorumi.nvim",
-        config = function()
-            vim.cmd("colorscheme yorumi")
-        end
-    },
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {
-        "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
-    },
-    {
-        "ThePrimeagen/harpoon",
-        branch = "harpoon2",
-        dependencies = { "nvim-lua/plenary.nvim" }
-    }
+  "nvim-lua/plenary.nvim",
+  'tpope/vim-fugitive',
+  'prettier/vim-prettier',
+  {
+    "yorumicolors/yorumi.nvim",
+    config = function()
+      vim.cmd("colorscheme yorumi")
+    end
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" }
+  }
 }
-
---
+++ lua/ashrock/lazy/treesitter.lua
@@ -0,0 +1,22 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "javascript", "typescript", "lua", "astro"
+      },
+
+      sync_install = false,
+      auto_install = true,
+
+      indent = {
+        enable = true
+      },
+      highlight = {
+        enable = true,
+        disable = { "astro" },
+      },
+    })
+  end
+}
