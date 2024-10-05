92e0505658c5643f5e62c2b884146dd15c7812d9
OnLee3 <dlwotjr2@gmail.com>
2024-10-05 18:26:56 +0900

    feat: lsp foramt only it has

--- lua/ashrock/init.lua
+++ lua/ashrock/init.lua
@@ -4,29 +4,31 @@ require("ashrock.remap")
 
 local autocmd = vim.api.nvim_create_autocmd
 autocmd('LspAttach', {
-    callback = function(e)
-        local opts = { buffer = e.buf }
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = e.buf,
-            callback = function()
-                vim.lsp.buf.format { async = false, id = e.data.client_id }
-            end,
-        })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            pattern = { "*.js", "*.jsx", "*.ts", "*.tsx" },
-            callback = function()
-                vim.cmd(":Prettier")
-            end,
-        })
+  callback = function(e)
+    local opts = { buffer = e.buf }
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    if vim.lsp.get_client_by_id(e.data.client_id).server_capabilities.documentFormattingProvider then
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        buffer = e.buf,
+        callback = function()
+          vim.lsp.buf.format { async = false, id = e.data.client_id }
+        end,
+      })
     end
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = { "*.js", "*.jsx", "*.ts", "*.tsx" },
+      callback = function()
+        vim.cmd(":Prettier")
+      end,
+    })
+  end
 })
