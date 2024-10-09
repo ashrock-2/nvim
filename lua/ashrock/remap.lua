4baeead2b49fd87094b1273a653a1409302a18b8
OnLee3 <dlwotjr2@gmail.com>
2024-10-10 07:14:22 +0900

    feat: remove arrow remap

--- lua/ashrock/remap.lua
+++ lua/ashrock/remap.lua
@@ -2,11 +2,6 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("i", "<C-h>", "<Left>")
-vim.keymap.set("i", "<C-j>", "<Down>")
-vim.keymap.set("i", "<C-k>", "<Up>")
-vim.keymap.set("i", "<C-l>", "<Right>")
-
 vim.keymap.set("n", "gh", "<cmd>diffget //2<CR>")
 vim.keymap.set("n", "gl", "<cmd>diffget //3<CR>")
 
