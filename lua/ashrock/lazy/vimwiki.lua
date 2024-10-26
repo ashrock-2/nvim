f5ad3717ebfb77219264d1ec9e124ace97d1fa72
OnLee3 <dlwotjr2@gmail.com>
2024-10-26 22:53:13 +0900

    chore: path update

--- lua/ashrock/lazy/vimwiki.lua
+++ lua/ashrock/lazy/vimwiki.lua
@@ -3,7 +3,8 @@ return {
   init = function()
     vim.g.vimwiki_list = {
       {
-        path = '~/Library/Mobile Documents/com~apple~CloudDocs/vimwiki',
+        path = '~/Documents/ashrock',
+        -- path = '~/Library/Mobile Documents/com~apple~CloudDocs/vimwiki',
         syntax = 'markdown',
         ext = '.md',
       },
