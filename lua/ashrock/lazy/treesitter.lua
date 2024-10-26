ea34db4d1cc8cc2bee34b5a37bd03ad6dc88bb9c
OnLee3 <dlwotjr2@gmail.com>
2024-10-27 06:42:05 +0900

    feat: update vimwiki path to icloud

--- lua/ashrock/lazy/treesitter.lua
+++ lua/ashrock/lazy/treesitter.lua
@@ -3,9 +3,9 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "javascript", "typescript", "lua", "astro", "tsx", "json", "markdown"
-      },
+      -- ensure_installed = {
+      --   "javascript", "typescript", "lua", "astro", "tsx", "json", "markdown"
+      -- },
 
       sync_install = false,
       auto_install = true,
