4d7ce4fc78e64f5f510450df6da661e799803d7c
OnLee3 <dlwotjr2@gmail.com>
2024-12-24 22:16:53 +0900

    refactor: LSP 설정 구조 개선 및 모듈화 - 각 LSP 관련 플러그인을 개별 테이블로 분리 - mason과 mason-lspconfig 설정을 독립적으로 구성 - capabilities 설정 로직 단순화 - lua_ls 설정에서 LuaJIT 버전 명시 - 불필요한 setup_capabilities 함수 제거

--- lua/ashrock/lazy/lsp.lua
+++ lua/ashrock/lazy/lsp.lua
@@ -1,47 +1,47 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
+  {
     "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "hrsh7th/cmp-nvim-lsp",
-  },
-  config = function()
-    local function setup_capabilities()
-      local cmp_lsp = require("cmp_nvim_lsp")
-      return vim.tbl_deep_extend(
-        "force",
-        {},
-        vim.lsp.protocol.make_client_capabilities(),
-        cmp_lsp.default_capabilities()
-      )
+    config = function()
+      require("mason").setup()
     end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        automatic_installation = true
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    local capabilities = setup_capabilities()
-    
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-      ensure_installed = { "lua_ls", "ts_ls" },
-      handlers = {
+      require("mason-lspconfig").setup_handlers({
         function(server_name)
-          require("lspconfig")[server_name].setup {
+          require("lspconfig")[server_name].setup({
             capabilities = capabilities
-          }
+          })
         end,
-        
+
         ["lua_ls"] = function()
-          require("lspconfig").lua_ls.setup {
+          require("lspconfig").lua_ls.setup({
             capabilities = capabilities,
             settings = {
               Lua = {
-                runtime = { version = "Lua 5.1" },
+                runtime = { version = "LuaJIT" },
                 diagnostics = {
-                  globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+                  globals = { "vim", "it", "describe", "before_each", "after_each" }
                 }
               }
             }
-          }
+          })
         end,
-      }
-    })
-  end
+      })
+    end
+  }
 }
