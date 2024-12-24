4a03d47549055d2afdb5b56db744c34eb5441f10
OnLee3 <dlwotjr2@gmail.com>
2024-12-24 22:18:55 +0900

    feat: ts_ls 서버 자동 설치 설정 추가 - mason-lspconfig에 TypeScript LSP 서버 ensure_installed 목록 추가 - 초기 설치 시 ts_ls 자동 설치되도록 구성

--- lua/ashrock/lazy/lsp.lua
+++ lua/ashrock/lazy/lsp.lua
@@ -9,6 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
+        ensure_installed = {"ts_ls" },
         automatic_installation = true
       })
     end
