50b009e2c808a155f9c1632f4c8a5e81c5f290a8
이재석 <jslee01@miridih.com>
2025-05-28 22:37:15 +0900

    파일 경로 길때 파일명이 잘리는 문제 해결

--- lua/ashrock/lazy/telescope.lua
+++ lua/ashrock/lazy/telescope.lua
@@ -3,5 +3,11 @@ return {
   tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
+    require('telescope').setup {
+      defaults = {
+        -- 긴 경로 중간을 … 으로 잘라내고, 끝부분(파일명)은 항상 보이게
+        path_display = { "truncate" },
+      },
+    }
   end
 }
